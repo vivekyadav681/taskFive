f580d90f32db5fe829a8f04630f994fd2576b79d
Vivek Yadav <vivekyadav00681@gmail.com>
2025-11-03 20:13:31 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/taskfive/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.taskfive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(body: Center(child: Text('CSI group task'))),
+    ),
+  );
+}
